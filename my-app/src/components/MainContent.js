868c8e5e6cbdbcf3505bced6a1c21c6f759ba30a
SuzanneNL <skr470@gmail.com>
2021-08-02 20:06:58 +0000

    Add inline styles

--- my-app/src/components/MainContent.js
+++ my-app/src/components/MainContent.js
@@ -5,12 +5,23 @@ function MainContent () {
     const lastPart = "my content";
     const date = new Date()
     let hours = date.getHours() 
-
+    const styles = {
+        color: "orange",
+        fontSize: 30
+    }
+    
+    if (hours > 21 || hours < 6) {
+        styles.backgroundColor = "blue"
+    }
+    else {
+        styles.backgroundColor = "yellow"
+    }
+    
 	return (
         <div>
             <p>Version 1: {firstPart + " " + lastPart}</p>
             <p>Version 2: {`${firstPart} ${lastPart}`}</p>
-            <p>It is currently {hours}.{date.getMinutes()}h</p>
+            <p style={styles}>It is currently {hours}.{date.getMinutes()}h</p>
         </div>
     )
 }
