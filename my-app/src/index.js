ac9cb648ada7f429cccf5f83f99f0db2377454c4
SuzanneNL <skr470@gmail.com>
2021-08-06 16:17:21 +0000

    Use componentDidMount for fetching data from an API and implement a loading feature

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import ToDoApp from './ToDoApp';
+import FetchingData from './FetchingData';
 
 ReactDOM.render(
-  <ToDoApp />,
+  <FetchingData />,
   document.getElementById("root")
   )
 
