fcd3584303c935408609f318cc897d9da7562c1b
SuzanneNL <skr470@gmail.com>
2021-08-06 18:30:12 +0000

    Form with textarea, checkbox, radio buttons and select boxes

--- my-app/src/Forms.js
+++ my-app/src/Forms.js
@@ -1,42 +1,97 @@
-import React, {Component} from 'react'
+import React, {Component} from "react"
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
             firstName: "",
-            lastName: ""
+            lastName: "",
+            isFriendly: true,
+            gender: "male",
+            favColor: "red"
         }
         this.handleChange = this.handleChange.bind(this)
     }
-
+    
     handleChange(event) {
-        const {name, value} = event.target
-        this.setState({
-            [name]: value
-        })
+        const {name, value, type, checked} = event.target
+        type === "checkbox" ? this.setState({[name]: checked}) : this.setState({[name]: value})
     }
+
+    handleSubmit(event) {
+        // Code for submitting here
+    }
+    
     render() {
         return (
-            <div>
-                <form>
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    type="text" 
+                    value={this.state.firstName} 
+                    name="firstName" 
+                    placeholder="First Name" 
+                    onChange={this.handleChange} 
+                />
+                <br />
+                <input 
+                    type="text" 
+                    value={this.state.lastName} 
+                    name="lastName" 
+                    placeholder="Last Name" 
+                    onChange={this.handleChange} 
+                />
+                <br />
+                <textarea 
+                    value={"Some default value"}
+                    onChange={this.handleChange}
+                />
+                <br />
+                <label>
                     <input 
-                        type="text" 
-                        value={this.state.firstName} 
-                        name="firstName" 
-                        placeholder="First Name" 
-                        onChange={this.handleChange}>
-                    </input>
+                        type="checkbox"
+                        name="isFriendly"
+                        checked={this.state.isFriendly}
+                        onChange={this.handleChange}
+                    /> Is friendly?
+                </label>
+                <br />
+                <label>
                     <input 
-                        type="text" 
-                        value={this.state.lastName} 
-                        name="lastName" 
-                        placeholder="Last Name" 
-                        onChange={this.handleChange}>
-                    </input>
-                </form>
-                <h1>{this.state.firstName} {this.state.lastName}</h1>
-            </div>
+                        type="radio"
+                        name="gender"
+                        value="male"
+                        checked={this.state.gender === "male"}
+                        onChange={this.handleChange}
+                    /> Male
+                </label>
+                <label>
+                    <input 
+                        type="radio"
+                        name="gender"
+                        value="female"
+                        checked={this.state.gender === "female"}
+                        onChange={this.handleChange}
+                    /> Female
+                </label>
+                <br />
+                <label>
+                    Your favorite color
+                    <br></br>
+                    <select
+                        value={this.state.favColor}
+                        onChange={this.handleChange}
+                        name="favColor"
+                    >
+                        <option value="blue">Blue</option>
+                        <option value="red">Red</option>
+                        <option value="yellow">Yellow</option>
+                    </select>
+                </label>
+
+                <h3>{this.state.firstName} {this.state.lastName}</h3>
+                <h3>You are a {this.state.gender} and your favorite color is {this.state.favColor}</h3>
+                <button>Submit</button>
+            </form>
         )
     }
 }
