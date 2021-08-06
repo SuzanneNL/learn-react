434ab52a6373fcbbe1d47fdd3c07cee15c364969
SuzanneNL <skr470@gmail.com>
2021-08-06 16:41:21 +0000

    Forms with text input

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import FetchingData from './FetchingData';
+import Forms from './Forms';
 
 ReactDOM.render(
-  <FetchingData />,
+  <Forms />,
   document.getElementById("root")
   )
 
